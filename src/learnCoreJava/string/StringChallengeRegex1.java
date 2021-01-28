8d3ca604a3e52a7b5bd78827449e25960dce0126
Breno Goya <breno.goya@gmail.com>
2021-01-28 12:22:13 -0300

    String Challenge Regex 1

---
+++ src/learnCoreJava/string/StringChallengeRegex1.java
@@ -0,0 +1,23 @@
+package learnCoreJava.string;
+
+public class StringChallengeRegex1 {
+
+	public static void main(String[] args) {
+		
+		int bin = 10;		
+		String hexaDecimal = "AB39AD29";
+		String str = String.valueOf(bin);
+		String d = "01/12/2021";		
+		
+		System.out.println(str);
+		System.out.println(str.matches("[01]+"));
+		System.out.println(hexaDecimal);
+		System.out.println(hexaDecimal.matches("[0-9A-F]+"));
+		System.out.println(d);
+		System.out.println(d.matches("[0-3][0-9]/[01][0-9]/[0-9]{4}"));
+	
+		
+		
+	}
+
+}
